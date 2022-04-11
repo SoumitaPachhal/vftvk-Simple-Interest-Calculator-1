90adbc892426b088d28c02fd4a58ffe2754e54f3
SoumitaPachhal <79303277+SoumitaPachhal@users.noreply.github.com>
2022-04-11 23:49:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@
  var interest=(p*t*r)/100;
      var total=interest + principal;
     
- document.getElementById('principal').innerHTML=p;
-     document.getElementById('rate').innerHTML=r;
-     document.getElementById('result').innerHTML=interest;
-     document.getElementById('result').innerHTML=total;
+ document.getElementById('output1').innerHTML=p;
+     document.getElementById('output2').innerHTML=r;
+     document.getElementById('output3').innerHTML=interest;
+     document.getElementById('output4').innerHTML=total;
 }
