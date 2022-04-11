c780c8a1ffe331212278ce4b62d9bd635a132719
SoumitaPachhal <79303277+SoumitaPachhal@users.noreply.github.com>
2022-04-11 23:05:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-function compute()
+     function compute()
 {
     p = document.getElementById("principal").value;
     t = document.getElementById("number").value;
     r = document.getElementById("rate").value;
-    result=(p*t*r)/100;
-    document.getElementById('result').innerHTML="result";
+ Interest=(p*t*r)/100;
     
+ document.getElementById('result').innerHTML=interest;
 }
-        
