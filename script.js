7bf1f705c518bb1dc33aa928fb27e201a90e3f76
SoumitaPachhal <79303277+SoumitaPachhal@users.noreply.github.com>
2022-04-11 18:00:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    t = document.getElementById("number").value;
+    r = document.getElementById("rate").value;
+    result=(p*t*r)/100;
+    document.getElementById('result').innerHTML="The Total Simple Intrest Is: "+result;
     
 }
         
